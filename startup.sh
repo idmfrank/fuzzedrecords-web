68412c98671e2b0c47790043fb3afba551d3edd2
idmfrank <idmfrank@gmail.com>
2024-12-17 11:23:21 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Ensure pip dependencies are installed
-pip install --upgrade pip
-pip install -r requirements.txt
+# Ensure dependencies are installed in the correct Python interpreter
+python3 --version
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt --target=/home/site/wwwroot
 
-# Start the Flask app using Gunicorn
+# Start the Flask app with Gunicorn
 gunicorn --bind=0.0.0.0 --timeout 600 app:app
