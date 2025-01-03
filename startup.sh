1dd453798b90be98bd243048718b9f252b72b5d8
idmfrank <idmfrank@gmail.com>
2025-01-03 14:15:48 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,9 +10,17 @@ fi
 # Activate the virtual environment
 source antenv/bin/activate
 
-# Upgrade pip and install requirements
+# Upgrade pip
 pip install --upgrade pip
-pip install -r requirements.txt --upgrade
+
+# Install dependencies
+if [ -d "wheels" ]; then
+    # Use cached wheels if available
+    pip install --no-index --find-links=./wheels -r requirements.txt
+else
+    # Otherwise, fetch from PyPI
+    pip install -r requirements.txt
+fi
 
 # Start Gunicorn and log output to stdout/stderr
 gunicorn --bind=0.0.0.0:8000 --log-level debug --access-logfile - --error-logfile - app:app
